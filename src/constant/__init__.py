3be66abb13fa2991ab74a2affecbb1402caf1447
Your Name <ankitachandrakar2301@gmail.com>
2025-01-16 15:34:33 +0530

    Final wafer-sensor-fault project

---
+++ src/constant/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "INVENTORY"
+MONGO_COLLECTION_NAME = "WAFERSENSOR"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://ankitachandrakar2301:1vtfsJ0VvcotX2W0@cluster0.o0z19.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
